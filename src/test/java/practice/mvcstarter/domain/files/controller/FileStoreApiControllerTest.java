df81ff6975a1279499f203de710b41d2ec864b84
JuJin1324 <jujin1324@daum.net>
2022-03-15 18:52:53 +0900

    [설계] 프로젝트 진행 순서 및 체크리스트 정의

--- src/test/java/practice/mvcstarter/domain/files/controller/FileStoreApiControllerTest.java
+++ src/test/java/practice/mvcstarter/domain/files/controller/FileStoreApiControllerTest.java
@@ -1,4 +1,4 @@
-package practice.mvcstarter.web.controllers.files;
+package practice.mvcstarter.domain.files.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
--- src/test/java/practice/mvcstarter/domain/members/controller/MemberApiControllerTest.java
+++ src/test/java/practice/mvcstarter/domain/members/controller/MemberApiControllerTest.java
@@ -1,4 +1,4 @@
-package practice.mvcstarter.web.controllers.members;
+package practice.mvcstarter.domain.members.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
